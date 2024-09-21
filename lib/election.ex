6b7585c60c68590e83828442035ed9fe024feea0
Gabo <gabrielomar2809@gmail.com>
2024-09-21 12:50:35 -0700

    Add view functions for header and body

--- lib/election.ex
+++ lib/election.ex
@@ -7,4 +7,36 @@ defmodule Election do
     ],
     next_id: 3
   )
+
+  def view_header(election) do
+    # "Election for: #{election.name}\n"
+    [
+      "Election for: #{election.name}\n"
+    ]
+  end
+
+  def view_body(election) do
+    election.candidates
+    |> sort_candidates_by_votes_desc()
+    |> candidates_to_string()
+    |> prepend_candidates_header()
+  end
+
+  defp sort_candidates_by_votes_desc(candidates) do
+    Enum.sort(candidates, &(&1.votes >= &2.votes))
+  end
+
+  defp candidates_to_string(candidates) do
+    Enum.map(candidates, fn %{id: id, name: name, votes: votes} ->
+      "#{id}\t#{votes}\t#{name}\n"
+    end)
+  end
+
+  defp prepend_candidates_header(candidates) do
+    [
+      "ID\tVotes\tName\n",
+      "--------------------------\n"
+      | candidates
+    ]
+  end
 end
